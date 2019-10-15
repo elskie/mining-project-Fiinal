7329b871035383de527b5903a8a25f2e2f9280da
elskie <55069033+elskie@users.noreply.github.com>
2019-10-15 18:33:08 -0500

    Add files via upload

---
+++ CubeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{   
+    public Ore myOre;// from the Ore enum, myOre stores either bronze,silver, or gold onto each individual cube
+    public Color initialColor;
+    void Start()
+    {
+        initialColor = GetComponent<Renderer>().material.color;//the material of the cube in unity is the initial color
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+     void OnMouseOver()
+     {
+         if (myOre == Ore.Bronze)
+         {
+            GetComponent<Renderer>().material.color = Color.red;
+         }
+        if (myOre == Ore.Gold)
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        if (myOre == Ore.Silver)
+        {
+            GetComponent<Renderer>().material.color = Color.gray;
+        }
+    }
+    void OnMouseExit()
+    {
+            GetComponent<Renderer>().material.color = initialColor;
+    }
+    void OnMouseDown() {
+        Destroy (gameObject); //destroys the object in the game and not the components of the object
+        //this is on every cube object
+        if (myOre == Ore.Bronze)//if from the ore enum list, a cube is bronze, and it its destroyed, then subjtract 1 from the ammount of bronze onscreen
+        {
+            GameControl.bronze--;
+            GameControl.bronzeSupply--;
+            GameControl.Score += GameControl.bronzePoints;
+        } 
+        else if (myOre == Ore.Silver)
+        {
+            GameControl.silver--;
+            GameControl.silverSupply--;
+            GameControl.Score += GameControl.silverPoints;
+        }
+
+        if (myOre == Ore.Gold)
+        {
+            GameControl.gold--;
+            GameControl.goldSupply--;
+            GameControl.Score += GameControl.goldPoints;
+        }//this is what indicates that the current ammount of cubes out should be subbtracted by 1 every time one is destroyed
+        
+    }
+}
---
+++ GameControl.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+	public static int bronze, silver, gold;// i will reference these integers later and in other scripts, keeps track of all all in the world
+	float miningSpeed, MineTime;
+	public static int bronzeSupply, silverSupply, goldSupply;
+    public GameObject BronzecubePrefab, SilvercubePrefab, GoldcubePrefab;
+    GameObject myCube;
+    Vector3 cubePosition;
+    float yPos, xPos;
+    Ore recentOre;
+    int space = 0;
+    public static int Score, bronzePoints, silverPoints, goldPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+		miningSpeed = 3;//how many seconds pass before mining again
+        MineTime = miningSpeed; //a variable to act as a way to continuously mine at the miningSpeed
+        bronzeSupply = 0;
+		silverSupply = 0;
+		goldSupply = 0;
+        yPos = -3;
+        xPos = -8;
+        recentOre = Ore.Bronze;
+        bronzePoints = 1;
+        silverPoints = 10;
+        goldPoints = 100;
+       // cubePosition = new Vector3(xPos, yPos, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Time.time>MineTime)
+        {
+			MineTime += miningSpeed;//if 1 sec pass, MineTime will activate and begin the taking from the supply, this will occur again after another miningSpeed seconds has passed
+
+            if (bronzeSupply == 2 && silverSupply == 2 && recentOre != Ore.Gold)
+            {
+                goldSupply++;
+                cubePosition = new Vector3(xPos, yPos, 0);
+                myCube = Instantiate(GoldcubePrefab, cubePosition, Quaternion.identity);
+                myCube.GetComponent<CubeController>().myOre = Ore.Gold;// when calling GenerateCube, You call on a type of ore from Ore enum and specify in the parameters which kind of Ore it is with generateOre
+                xPos += 2;
+                recentOre = Ore.Gold;
+            }
+
+            else if (bronzeSupply < 4)
+            {
+				bronzeSupply ++;
+                cubePosition = new Vector3(xPos, yPos, 0);
+                myCube = Instantiate(BronzecubePrefab, cubePosition, Quaternion.identity);
+                myCube.GetComponent<CubeController>().myOre = Ore.Bronze;// when calling GenerateCube, You call on a type of ore from Ore enum and specify in the parameters which kind of Ore it is with generateOre
+                xPos += 2;
+                recentOre = Ore.Bronze;
+            }
+
+			else
+            {	silverSupply ++;
+                cubePosition = new Vector3(xPos, yPos, 0);
+                myCube = Instantiate(SilvercubePrefab, cubePosition, Quaternion.identity);
+                myCube.GetComponent<CubeController>().myOre = Ore.Silver;// when calling GenerateCube, You call on a type of ore from Ore enum and specify in the parameters which kind of Ore it is with generateOre
+                xPos += 2;
+                recentOre = Ore.Silver;
+
+               if (xPos > 8 && space == 0)
+                {
+                    xPos = -7;
+                    yPos += 1;
+                    space = 1;
+                }
+               else if (xPos > 8 && space == 1)
+                {
+                    xPos = -8;
+                    yPos += 1;
+                    space = 0;
+                }
+            }
+
+            print("Total Points: " +Score);
+        }
+    }
+}
